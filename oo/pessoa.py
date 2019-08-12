2b80689018d12a2c8b28636fdbd9f035b5e22a23
Lucas Braga <lucas_santos_braga@hotmail.com>
2019-08-12 19:25:35 -0400

    Criado atributo complexo filhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,17 +1,23 @@
 class Pessoa:
-    def __init__(self, nome=None, idade=35):
+    def __init__(self, *filhos, nome=None, idade=50):
         self.idade = idade
         self.nome = nome
+        self.filhos = list(filhos)
+
 
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 if __name__ == '__main__':
-    p = Pessoa('Lucas')
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
-    print(p.nome)
-    p.nome = 'Mudamos'
-    print(p.nome)
-    print(p.idade)
+    lucas = Pessoa(nome='Lucas')
+    marcos = Pessoa(nome='Marcos')
+    joao = Pessoa(lucas, marcos, nome='João Batista')
+
+    print(Pessoa.cumprimentar(joao))
+    print(id(lucas))
+    print(joao.cumprimentar(),'\n')
+
+    print(joao.nome)
+    print(joao.idade)
+    for filho in joao.filhos:
+        print(filho.nome)
