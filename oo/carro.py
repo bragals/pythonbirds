040eccdf63b429c71502a5b5c17d73fd5d3dd823
Lucas Braga <lucas_santos_braga@hotmail.com>
2019-08-17 06:21:47 -0400

    Implementada Direção

--- oo/carro.py
+++ oo/carro.py
@@ -41,6 +41,9 @@ Exemplo:
     >>> motor.frear()
     >>> motor.velocidade
     1
+    >>> motor.frear()
+    >>> motor.velocidade
+    0
     >>> # Testando Direcao
     >>> direcao = Direcao()
     >>> direcao.valor
@@ -51,6 +54,47 @@ Exemplo:
     >>> direcao.girar_a_direita()
     >>> direcao.valor
     'Sul'
+    >>> direcao.girar_a_direita()
+    >>> direcao.valor
+    'Oeste'
+    >>> direcao.girar_a_direita()
+    >>> direcao.valor
+    'Norte'
+    >>> direcao.girar_a_esquerda()
+    >>> direcao.valor
+    'Oeste'
+    >>> direcao.girar_a_esquerda()
+    >>> direcao.valor
+    'Sul'
+    >>> direcao.girar_a_esquerda()
+    >>> direcao.valor
+    'Leste'
+    >>> direcao.girar_a_esquerda()
+    >>> direcao.valor
+    'Norte'
+    >>> carro = Carro(direcao, motor)
+    >>> carro.calcular_velocidade()
+    0
+    >>> carro.acelerar()
+    >>> carro.calcular_velocidade()
+    1
+    >>> carro.acelerar()
+    >>> carro.calcular_velocidade()
+    2
+    >>> carro.frear()
+    >>> carro.calcular_velocidade()
+    0
+    >>> carro.calcular_direcao()
+    'Norte'
+    >>> carro.girar_a_direita()
+    >>> carro.calcular_direcao()
+    'Leste'
+    >>> carro.girar_a_esquerda()
+    >>> carro.calcular_direcao()
+    'Norte'
+    >>> carro.girar_a_esquerda()
+    >>> carro.calcular_direcao()
+    'Oeste'
  """
 
 
@@ -61,8 +105,8 @@ class Carro:
 
 
 class Motor:
-    def __init__(self, velocidade=0):
-        self.velocidade = velocidade
+    def __init__(self):
+        self.velocidade = 0
 
     def acelerar(self):
         self.velocidade += 1
@@ -70,24 +114,28 @@ class Motor:
 
     def frear(self):
         self.velocidade -= 2
+        self.velocidade = max(0, self.velocidade)
 
+NORTE = 'Norte'
+SUL = 'Sul'
+LESTE = 'Leste'
+OESTE = 'Oeste'
 
 class Direcao:
-    direcao = ['Norte', ]
-    def __init__(self, valor):
-        self.valor = valor
+    rotacao_a_direita_dct = {
+        NORTE: LESTE, LESTE: SUL, SUL: OESTE, OESTE: NORTE
+    }
+    rotacao_a_esquerda_dct = {
+        NORTE: OESTE, OESTE: SUL, SUL: LESTE, LESTE: NORTE
+    }
 
-
+    def __init__(self):
+        self.valor  = NORTE
 
     def girar_a_direita(self):
-        self.valor = 'Leste'
+        self.valor = self.rotacao_a_direita_dct[self.valor]
 
     def girar_a_esquerda(self):
-        pass
+        self.valor = self.rotacao_a_esquerda_dct[self.valor]
 
-
-
-
-
-
-
+# python3 -m doctest script.py -f
