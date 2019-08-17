de56325e1e8d2d79d44fb45be166d16f762bc91a
Lucas Braga <lucas_santos_braga@hotmail.com>
2019-08-16 20:54:10 -0400

    exercício composição carro lucas's implementation

--- oo/carro.py
+++ oo/carro.py
@@ -41,9 +41,6 @@ Exemplo:
     >>> motor.frear()
     >>> motor.velocidade
     1
-    >>> motor.frear()
-    >>> motor.velocidade
-    0
     >>> # Testando Direcao
     >>> direcao = Direcao()
     >>> direcao.valor
@@ -54,46 +51,43 @@ Exemplo:
     >>> direcao.girar_a_direita()
     >>> direcao.valor
     'Sul'
-    >>> direcao.girar_a_direita()
-    >>> direcao.valor
-    'Oeste'
-    >>> direcao.girar_a_direita()
-    >>> direcao.valor
-    'Norte'
-    >>> direcao.girar_a_esquerda()
-    >>> direcao.valor
-    'Oeste'
-    >>> direcao.girar_a_esquerda()
-    >>> direcao.valor
-    'Sul'
-    >>> direcao.girar_a_esquerda()
-    >>> direcao.valor
-    'Leste'
-    >>> direcao.girar_a_esquerda()
-    >>> direcao.valor
-    'Norte'
-    >>> carro = Carro(direcao, motor)
-    >>> carro.calcular_velocidade()
-    0
-    >>> carro.acelerar()
-    >>> carro.calcular_velocidade()
-    1
-    >>> carro.acelerar()
-    >>> carro.calcular_velocidade()
-    2
-    >>> carro.frear()
-    >>> carro.calcular_velocidade()
-    0
-    >>> carro.calcular_direcao()
-    'Norte'
-    >>> carro.girar_a_direita()
-    >>> carro.calcular_direcao()
-    'Leste'
-    >>> carro.girar_a_esquerda()
-    >>> carro.calcular_direcao()
-    'Norte'
-    >>> carro.girar_a_esquerda()
-    >>> carro.calcular_direcao()
-    'Oeste'
-
  """
+
+
+class Carro:
+    def __init__(self, motor, direcao):
+        self.motor = motor
+        self.direcao = direcao
+
+
+class Motor:
+    def __init__(self, velocidade=0):
+        self.velocidade = velocidade
+
+    def acelerar(self):
+        self.velocidade += 1
+
+
+    def frear(self):
+        self.velocidade -= 2
+
+
+class Direcao:
+    direcao = ['Norte', ]
+    def __init__(self, valor):
+        self.valor = valor
+
+
+
+    def girar_a_direita(self):
+        self.valor = 'Leste'
+
+    def girar_a_esquerda(self):
+        pass
+
+
+
+
+
+
+
